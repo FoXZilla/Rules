960f887ecb723fd7b157e0ea2c9e2075b5982408
花生PeA <pea3nut@qq.com>
2019-02-23 14:29:38 +0800

    change webpack config for fix git-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
-const Webpack = require('webpack');
 const Path = require('path');
+const Webpack = require('webpack');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
-module.exports = (env, argv) => {
-    return {
-        entry: './src/index.ts',
-        output: {
-            path: Path.join(__dirname, 'dist/'),
-            filename: 'rules.js',
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    enforce: 'pre',
-                    use: 'tslint-loader',
-                },
-                { test: /\.ts$/, use: 'ts-loader' },
-                { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-            ],
-        },
-        resolve: { extensions: ['.js', '.ts'] },
-        plugins: [
-            new Webpack.DefinePlugin({
-                'process.env.NODE_ENV': JSON.stringify(argv.mode),
-            }),
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: Path.join(__dirname, 'dist/'),
+        filename: 'rules.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                enforce: 'pre',
+                use: 'tslint-loader',
+            },
+            { test: /\.ts$/, use: 'ts-loader' },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
         ],
-    }
+    },
+    resolve: { extensions: ['.js', '.ts'] },
+    plugins: [
+        new FileManagerPlugin({
+            onEnd: {
+                copy: [{
+                    source: 'dist/rules.js',
+                    destination: 'docs/rules.js',
+                }],
+            },
+        }),
+    ],
 };
